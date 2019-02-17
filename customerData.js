e6bfd6c8677c8af964b9109d25d2cedfffa9af51
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro-2.local>
2019-02-17 17:24:10 -0500

    attempting to resolve pending promise issue

--- customerData.js
+++ customerData.js
@@ -5,33 +5,73 @@ const csvFilePath= './Data.csv'
 const csv = require('csvtojson')
 
 
-// utilized the csvtojson npm package
+
+
 let customerDataJSON= (
 csv()
 	.fromFile(csvFilePath)
 	.then((jsonObj)=>{
-		// console.log(jsonObj)
 		return(jsonObj)
-	}))
+		console.log(jsonObj)
+}))
+
 
 // refractor JSON parameters to match Kustomer required parameters
 let refractoredData = async()=>{
-	let newData = await customerDataJSON.map(obj => {
-		// console.log(`${obj.firstName} ${obj.lastName}`)
-		obj = {...obj, firstName: `${obj.firstName} ${obj.lastName}`}
-		// console.log(obj)
-		
-		obj['name']= obj['firstName']
-		delete obj['firstName']
-		delete obj['lastName']
-		
-		return obj
-
-	})
-	// console.log('THIS IS NEW DATA: ', newData)
+	try{ 
+		let newData = customerDataJSON ? '' : customerDataJSON.map(obj => {
+			// console.log(`${obj.firstName} ${obj.lastName}`)
+			obj = {...obj, firstName: `${obj.firstName} ${obj.lastName}`}
+			// console.log(obj)
+			
+			obj['name']= obj['firstName']
+			delete obj['firstName']
+			delete obj['lastName']
+			
+			return Promise.resolve(obj)
+		})
+		// console.log('THIS IS NEW DATA: ', newData)
+	}
+	catch(e){
+		return ('failed')
+	}
+	
 }
 
-refractoredData()
+let callback = refractoredData()
+
+// function Data() {
+// 	let customerDataJSON= (
+// 		csv()
+// 			.fromFile(csvFilePath)
+// 			.then((jsonObj)=>{
+// 				return(jsonObj)
+// 	}))
+
+// 	this.customerData = customerDataJSON,
+
+// 	this.refractoredData= async()=>{
+// 		try{
+// 			let newData = await customerDataJSON.map(obj => {
+// 				obj = {...obj, firstName: `${obj.firstName} ${obj.lastName}`}
+
+// 				obj['name']= obj['firstName']
+// 				delete obj['firstName']
+// 				delete obj['lastName']
+				
+// 				return obj
+// 			})
+// 			// console.log('THIS IS NEW DATA: ', newData)
+// 		}
+// 		catch(e){
+// 			return ('failed')
+// 		}
+		
+// 	}
+// 	}
+	
+
+// const data = new Data()
 
 
-module.exports = refractoredData
+module.exports = callback
--- script.js
+++ script.js
@@ -3,26 +3,109 @@
 // required inputs
 const axios = require('axios')
 const apiKey = require('./apiKey.js')
-const customerData = require('./customerData.js')
+const csvFilePath= './Data.csv'
+const csv = require('csvtojson')
+
+
+
+// utilized the csvtojson npm package
+
+let customerDataJSON= (
+	csv()
+	.fromFile(csvFilePath)
+	.then((jsonObj)=>{
+		// console.log(jsonObj)
+		// let string = JSON.stringify(jsonObj)
+		// console.log(string)
+		// console.log('WORKS', jsonObj)
+		return(jsonObj)
+	})
+	.catch((e) =>{
+		console.log("REJECTED")
+	})
+)
+
+
+// refractor JSON parameters to match Kustomer required parameters
+
+let refractoredData = async() =>{
+	let newData;
+
+	try{ 
+		newData = await customerDataJSON.map(obj => {
+			// console.log(`${obj.firstName} ${obj.lastName}`)
+			obj = {...obj, firstName: `${obj.firstName} ${obj.lastName}`}
+			// console.log(obj)
+			
+			obj['name']= obj['firstName']
+			delete obj['firstName']
+			delete obj['lastName']
+			
+			let string = JSON.stringify(obj)
+			// console.log(string)
+			return string
+		})
+	}
+	catch(e){
+		return ('failed')
+	}
+	
+	console.log('THIS IS NEW DATA: ', newData)
+	
+	return newData
+}
+
+let data = (refractoredData()
+	.then(result => {
+	  return result
+	}).catch(error => {
+	  return 'error'
+	}))
+
+console.log('THIS IS CALLBACk', data)
 
 
 // Post converted data to Kustomer API through axios npm package
-axios.post('https://api.kustomerapp.com/v1/customers', {
-    customerData
-  },
-  {
-  	headers: {
-  		"Content-Type": "application/json",
-  		"Authorization": `Bearer ${apiKey}`
-  	}
-  })
-  .then(function (response) {
-    // console.log('RESPONSE: ', response.status, response.statusText);
-    if(response.status === 201){
-    	console.log('Data successfully sent to Kustomer')
-    }
-  })
-  .catch(function (error) {
-    console.log('ERROR: ', error);
-  });
+
+console.log('OBJECT KEYS', Object.keys(data))
+// Object.keys(customerData).forEach(name =>{
+// 	console.log(name)
+// 	axios.post('https://api.kustomerapp.com/v1/customers', {
+//     name
+//   },
+//   {
+//   	headers: {
+//   		"Content-Type": "application/json",
+//   		"Authorization": `Bearer ${apiKey}`
+//   	}
+//   })
+//   .then(function (response) {
+//     console.log('RESPONSE: ', response.data.data);
+//     if(response.status === 201){
+//     	console.log('Data successfully sent to Kustomer')
+//     }
+//   })
+//   .catch(function (error) {
+//     console.log('ERROR: ', error);
+//   });
+// })
+
+// axios.post('https://api.kustomerapp.com/v1/customers', {
+//     callback
+//   },
+//   {
+//   	headers: {
+//   		"Content-Type": "application/json",
+//   		"Authorization": `Bearer ${apiKey}`
+//   	}
+//   })
+//   .then(function (response) {
+//     console.log('RESPONSE: ', response.data);
+//     if(response.status === 201){
+//     	console.log('Data successfully sent to Kustomer')
+//     }
+//   })
+//   .catch(function (error) {
+//     console.log('ERROR: ', error);
+//   });
 
